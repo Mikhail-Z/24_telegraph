097c3745facfa49248b8011c9dac8292d70f49ab
mikhail-z <09041996a>
2018-01-25 22:39:34 +0300

    config for heroku

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import psycopg2
 DEBUG = True
 
 # для production
-# SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 # для local
-SQLALCHEMY_DATABASE_URI = 'sqlite:///anongraph.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///anongraph.db'
 
 
 SECRET_KEY = 'thisissecretkey'
